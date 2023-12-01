00eae9d33e609c7943be96c02efeec8e3345b99d
Leonardo Negreiro <negreiroleo@gmail.com>
2023-11-30 23:50:41 -0300

    modificando componentes.

--- my-app/src/pages/Welcome/index.js
+++ my-app/src/pages/Welcome/index.js
@@ -73,7 +73,7 @@ const styles = StyleSheet.create({
 
   buttonText: {
     width: '140px',
-    color: '#39DAFF',
+    color: '#FFFF',
     textalign: 'center',
     fontfamily: 'Segoe UI',
     fontsize: '16px',
